4aa27059a0fc872f9264c9d51693b5cd351e475b
Samuel <themainsamuel@gmail.com>
2024-06-25 16:18:44 +0100

    it works!!

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,37 +8,42 @@ import unittest
 
 class TestBaseModel(unittest.TestCase):
     """contains test functions for `BaseModel`"""
+
+    def setUp(self):
+        self.bm1 = BaseModel()
+        self.bm1.name = "Model 1"
+        self.bm2 = BaseModel()
+        self.bm2.name = "Model 2"
+
+    def tearDown(self):
+        del self.bm1
+        del self.bm2
+
     def test_uuid(self):
         """tests the uniqueness of instance ids"""
-        b1 = BaseModel()
-        b2 = BaseModel()
-        self.assertIsInstance(b1, BaseModel)
-        self.assertNotEqual(b1.id, b2.id)
-        self.assertTrue(hasattr(b1, "id"))
-        self.assertIsInstance(b1.id, str)
+        self.assertIsInstance(self.bm1, BaseModel)
+        self.assertTrue(hasattr(self.bm1, "id"))
+        self.assertNotEqual(self.bm1.id, self.bm2.id)
+        self.assertIsInstance(self.bm1.id, str)
 
     def test_str(self):
         """tests for __str__ function"""
-        b1 = BaseModel()
-        str_rep = str(b1)
-        self.assertTrue(str(b1))
-        self.assertEqual(str_rep, str(b1))
-        self.assertIsNotNone(str_rep)
-        self.assertIsInstance(str(b1), str)
+        str_rep = str(self.bm1)
+        expected_str_rep = \
+            f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        self.assertEqual(str_rep, str(self.bm1))
 
     def test_save(self):
         """tests the save function"""
-        b1 = BaseModel()
-        updated_at = b1.save()
-        self.assertEqual(updated_at, b1.updated_at)
-        self.assertIsNotNone(b1.save())
-        self.assertIsInstance(b1.updated_at, datetime.datetime)
+        updated_at = self.bm1.save()
+        self.assertEqual(updated_at, self.bm1.updated_at)
+        self.assertIsNotNone(self.bm1.save())
+        self.assertIsInstance(self.bm1.updated_at, datetime.datetime)
 
     def test_to_dict(self):
         """tests the to_dict function"""
-        b1 = BaseModel()
-        instance_dict_rep = b1.to_dict()
+        instance_dict_rep = self.bm1.to_dict()
         self.assertIsInstance(instance_dict_rep, dict)
         self.assertIsInstance(instance_dict_rep["created_at"], str)
         self.assertIsInstance(instance_dict_rep["updated_at"], str)
-        self.assertIsNotNone(b1.to_dict())
+        self.assertIsNotNone(self.bm1.to_dict())
