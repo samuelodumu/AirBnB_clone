6f24ee6a6f6f26d49f7677c653728496d6c3638d
Samuel <themainsamuel@gmail.com>
2024-06-25 13:06:28 +0100

    fix: pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """Contains `BaseModel` class"""
 
-import datetime
+import datetime as dt
 import json
 import uuid
+
 
 class BaseModel:
     """defines all common attributes/methods for other classes"""
     def __init__(self):
         """Initializing instance ids"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = dt.datetime.now()
+        self.updated_at = dt.datetime.now()
 
     def __str__(self):
         """string representation"""
@@ -19,13 +20,13 @@ class BaseModel:
 
     def save(self):
         """updates `updated_at` attribute with the current datetime"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = dt.datetime.now()
         return self.updated_at
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """returns a dictionary containing all keys/values of the instance"""
         instance_dict_rep = self.__dict__.copy()
-        instance_dict_rep["created_at"] = datetime.datetime.isoformat(self.created_at)
-        instance_dict_rep["updated_at"] = datetime.datetime.isoformat(self.updated_at)
+        instance_dict_rep["created_at"] = self.created_at.isoformat()
+        instance_dict_rep["updated_at"] = self.updated_at.isoformat()
         instance_dict_rep["__class__"] = self.__class__.__name__
         return instance_dict_rep
