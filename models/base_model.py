11228cc8217b24e4675599840bc4ed2ed130bdf7
Samuel <themainsamuel@gmail.com>
2024-07-19 15:54:57 +0100

    now unto the tests

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,7 @@
 
 import datetime as dt
 import json
-from models import storage
-from models.engine.file_storage import FileStorage
+import models
 import uuid
 
 
@@ -16,7 +15,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = dt.datetime.now()
             self.updated_at = dt.datetime.now()
-            storage.new(self)
+            models.storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key in ['id', 'created_at', 'updated_at']:
@@ -40,9 +39,8 @@ class BaseModel:
 
     def save(self):
         """updates `updated_at` attribute with the current datetime"""
-        storage.save()
         self.updated_at = dt.datetime.now()
-        return self.updated_at
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of the instance"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """contains `FileStorage` class"""
-
 import json
 import os
+from models.base_model import BaseModel
 
 
 class FileStorage:
@@ -22,8 +22,21 @@ class FileStorage:
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
+        existing_data = {}
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
+                try:
+                    existing_data = json.load(f)
+                except json.JSONDecodeError:
+                    pass
+                FileStorage.__objects.update(existing_data)
+
+        else:
+            FileStorage.__objects.update(existing_data)
+
         with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
             json.dump(FileStorage.__objects, f, indent=4)
+            # print(f"Type of existing_data: {type(existing_data)}")
 
     def reload(self):
         """deserializes the JSON file to __objects if __file_path exists"""
@@ -31,12 +44,17 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r', encoding="utf-8") as f:
                 try:
                     obj_dict = json.load(f)
-
                 except json.JSONDecodeError:
                     return
-                for key, value in FileStorage.__objects.items():
+
+                for key, value in obj_dict.items():
                     class_name = key.split(".")[0]
-                    obj = globals()[class_name](**obj_dict[key])
+                    obj = globals()[class_name](**value)
                     FileStorage.__objects[key] = obj
+
+                    # another way to do it if BaseModel or whatever class_name
+                    # is is not listed in the globals() dictionary.
+                    # if 'BaseModel' in key:
+                    #     FileStorage.__objects[key] = BaseModel(**value)
         except FileNotFoundError:
-            pass
+            return
