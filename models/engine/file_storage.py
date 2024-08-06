46f6673788a6e2d5e4496d798847b5f8e7dcc110
Samuel <themainsamuel@gmail.com>
2024-08-06 01:26:59 +0100

    the Lord has done it finally (understanding is key)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,25 +18,33 @@ class FileStorage:
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = f'{obj.__class__.__name__}.{obj.id}'
-        FileStorage.__objects[key] = obj.to_dict()
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         existing_data = {}
+        # turn each object to its JSON representation
+        for key, value in FileStorage.__objects.items():
+            FileStorage.__objects[key] = value.to_dict()
+        # print(f"FileStorage.__objects: {FileStorage.__objects}")
+
+        # check if file exists
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
+                # try deserializing the file into `existing_data`
+                # the whole point of this is to serialize both
+                # the existing data in the file as well as the
+                # newly defined objects in `FileStorage.__objects`
                 try:
                     existing_data = json.load(f)
                 except json.JSONDecodeError:
                     pass
                 FileStorage.__objects.update(existing_data)
 
-        else:
-            FileStorage.__objects.update(existing_data)
-
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
-            json.dump(FileStorage.__objects, f)
-            # print(f"Type of existing_data: {type(existing_data)}")
+        with open(
+                FileStorage.__file_path, 'w', encoding='utf-8'
+                ) as f:
+            json.dump(FileStorage.__objects, f, indent=4)
 
     def reload(self):
         """deserializes the JSON file to __objects if __file_path exists"""
